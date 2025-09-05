0fb215ebdd39cde7fa7f4b67166736eee06fd633
DaoHai <Dhai9069@gmail.com>
2025-09-05 21:32:07 +0700

    YaMe

--- src/compondents/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from 'Header';
-export { default as Banner } from 'Banner';
-export { default as Cart_shop } from 'CartShop';
-export { default as Carousel } from 'carousel';
-export { default as Footer } from 'Footer';
